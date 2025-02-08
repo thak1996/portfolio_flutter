6445cbfabf29e179212e57bbdc754313ebca11ee
Franklyn Viana dos Santos <franklyn_vs_@hotmail.com>
2025-02-08 04:18:25 -0300

    adiciona componentes MainDesktop e MainMobile à HomePage para layout responsivo

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -4,6 +4,8 @@ import '../../core/constants/size.dart';
 import '../widgets/drawer_mobile.dart';
 import '../widgets/header_desktop.dart';
 import '../widgets/header_mobile.dart';
+import '../widgets/main_desktop.dart';
+import '../widgets/main_mobile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,6 +18,9 @@ class _HomePageState extends State<HomePage> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    final screenHeight = screenSize.height - kToolbarHeight;
+    final screenWidth = screenSize.width;
     return LayoutBuilder(
       builder: (context, constraints) => Scaffold(
         key: scaffoldKey,
@@ -26,27 +31,31 @@ class _HomePageState extends State<HomePage> {
           scrollDirection: Axis.vertical,
           children: [
             // Main
-            if (constraints.maxWidth >= kMinWidthDesktop)
-              HeaderDesktop()
-            else
-              HeaderMobile(
-                onLogoTap: () {},
-                onMenuTap: () => scaffoldKey.currentState?.openEndDrawer(),
-              ),
+            constraints.maxWidth >= kMinWidthDesktop
+                ? HeaderDesktop()
+                : HeaderMobile(
+                    onLogoTap: () {},
+                    onMenuTap: () => scaffoldKey.currentState?.openEndDrawer(),
+                  ),
+            constraints.maxWidth >= kMinWidthDesktop
+                ? MainDesktop()
+                : MainMobile(),
+
             // Skills
-            SizedBox(
-              height: 500,
+            Container(
+              height: screenSize.height,
               width: double.maxFinite,
+              color: Colors.red,
             ),
             // Projects
             Container(
-              color: Colors.green,
-              height: 500,
+              height: screenSize.height,
               width: double.maxFinite,
+              color: Colors.green,
             ),
             // Contact
             SizedBox(
-              height: 500,
+              height: screenSize.height,
               width: double.maxFinite,
             ),
           ],
